001f021640ab123f5c0be7cb5957c8e6c387333a
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-24 11:16:07 +0200

    Make the general structure installable

---
+++ python/dune/__init__.py
@@ -0,0 +1,1 @@
+# The module dune collects all dune-related python modules
---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dune',
+      version='2.4',
+      description='Python package accompanying the DUNE project',
+      url='http://www.dune-project.org',
+      author='Dominic Kempf',
+      author_email='dominic.kempf@iwr.uni-heidelberg.de',
+      license='BSD',
+      packages=['dune', 'dune.common']
+     )
