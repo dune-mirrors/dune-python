9ac186cd1174805c09b4b0be7e71b39741849740
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-24 13:23:22 +0200

    Add a module that checks for the existence of packages on the host system

---
+++ cmake/modules/CheckPythonPackage.cmake
@@ -0,0 +1,35 @@
+# Find a given python package on the host system. Note, that you should
+# only use this macro in the context of dune-python if you really need the
+# package to be present on the host. Any dependencies of your python packages
+# will instead be installed into the dune-python virtualenv. dune-python
+# uses this module to check for the existence of python-virtualenv.
+#
+# check_python_package(PACKAGE package
+#                      [REQUIRED])
+#
+# Checks for the existence of the given package on the host system. The variable
+# DUNE_PYTHON_<package>_FOUND will be set correctly afterwards. If the REQUIRED
+# option is set, the function will error out.
+#
+
+function(check_python_package)
+  # Parse Arguments
+  set(OPTION REQUIRED)
+  set(SINGLE PACKAGE)
+  set(MULTI)
+  include(CMakeParseArguments)
+  cmake_parse_arguments(PYCHECK "${OPTION}" "${SINGLE}" "${MULTI}" ${ARGN})
+  if(PYCHECK_UNPARSED_ARGUMENTS)
+    message(WARNING "Unparsed arguments in check_python_package: This often indicates typos!")
+  endif()
+
+  execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import ${PYCHECK_PACKAGE}" RESULT_VARIABLE PYCHECK_RETURN)
+  if(PYCHECK_RETURN STREQUAL "0")
+    set(DUNE_PYTHON_${PYCHECK_PACKAGE}_FOUND TRUE PARENT_SCOPE)
+  else()
+    set(DUNE_PYTHON_${PYCHECK_PACKAGE}_FOUND FALSE PARENT_SCOPE)
+    if(PYCHECK_REQUIRED)
+      message(FATAL_ERROR "The python package ${PYCHECK_PACKAGE} could not be found on the host system, but is needed by the project ${CMAKE_PROJECT_NAME}")
+    endif()
+  endif()
+endfunction()
--- cmake/modules/DunePythonMacros.cmake
+++ cmake/modules/DunePythonMacros.cmake
@@ -1,5 +1,9 @@
-find_package(PythonInterp REQUIRED)
-
 # include all macros that dune-python offers. They can be documented better if placed
 # in cmake modules grouped together by functionality
+include(CheckPythonPackage)
+include(InstallPythonPackage)
 include(PythonVersion)
+
+# The code we do want to execute whenever a module that requires or suggests dune-pyhton is configured
+find_package(PythonInterp REQUIRED)
+check_python_package(PACKAGE virtualenv REQUIRED)
