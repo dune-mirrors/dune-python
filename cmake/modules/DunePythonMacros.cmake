09d3ccf35d57c06c3bf9c0c1ae2dd554abb8f224
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-24 14:19:29 +0200

    Major update
    The virtual env wrapper script is now correctly written
    both within dune-python and all downstream modules.

--- cmake/modules/DunePythonMacros.cmake
+++ cmake/modules/DunePythonMacros.cmake
@@ -1,9 +1,20 @@
 # include all macros that dune-python offers. They can be documented better if placed
 # in cmake modules grouped together by functionality
 include(CheckPythonPackage)
-include(InstallPythonPackage)
+include(DuneVirtualEnv)
 include(PythonVersion)
 
 # The code we do want to execute whenever a module that requires or suggests dune-pyhton is configured
 find_package(PythonInterp REQUIRED)
 check_python_package(PACKAGE virtualenv REQUIRED)
+
+# Write a wrapper for the virtualenv into the current build directory
+# TODO provide versions of this script that work on other platforms
+if(CMAKE_PROJECT_NAME STREQUAL dune-python)
+  set(DUNE_VIRTUALENV_PATH ${CMAKE_BINARY_DIR}/python-env)
+  set(DUNE_PYTHON_TEMPLATES_PATH ${CMAKE_SOURCE_DIR}/cmake/modules)
+else()
+  set(DUNE_VIRTUALENV_PATH ${dune-python_DIR}/python-env)
+  set(DUNE_PYTHON_TEMPLATES_PATH ${dune-python_MODULE_PATH})
+endif()
+configure_file(${DUNE_PYTHON_TEMPLATES_PATH}/env-wrapper.sh.in ${CMAKE_BINARY_DIR}/dune-env.sh)
---
+++ cmake/modules/DuneVirtualEnv.cmake
@@ -0,0 +1,24 @@
+# This cmake module provides macros that allow to access and modify the virtual env
+# that is shared by all dune modules, that depend on dune-python.
+#
+# dune_install_python_package(PATH path)
+#
+# Installs the python package located at path into the virtualenv used by dune-python
+# The package at the given location is expected to be a pip installable package.
+# TODO: also install it globally during make install.
+#
+
+function(dune_install_python_package)
+  # Parse Arguments
+  set(OPTION)
+  set(SINGLE PATH)
+  set(MULTI)
+  include(CMakeParseArguments)
+  cmake_parse_arguments(PYINST "${OPTION}" "${SINGLE}" "${MULTI}" ${ARGN})
+  if(PYINST_UNPARSED_ARGUMENTS)
+    message(WARNING "Unparsed arguments in dune_install_python_packge: This often indicates typos!")
+  endif()
+
+  # install the package
+  #execute_process()
+endfunction()
