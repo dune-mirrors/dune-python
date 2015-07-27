3f605783ad97105e9d47662e65ab63843719dd35
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-27 17:46:13 +0200

    Do not compare python2 and python3 versions to each other.

--- cmake/modules/PythonVersion.cmake
+++ cmake/modules/PythonVersion.cmake
@@ -3,10 +3,22 @@
 # dune_require_python_version(version)
 # 
 # Error out, if the python interpreter found by cmake is older than the given version.
+# Python2 and Pyhton3 versions are not compared to each other, so you can call this
+# macro multiple times to enforce minimal versions on a python2 and python3 interpreter
+# independently.
+#
 
 macro(dune_require_python_version version)
-  if(PYTHON_VERSION_STRING VERSION_LESS ${version})
-    message(FATAL_ERROR "${CMAKE_PROJECT_NAME} requires at least python ${version}")
+  if(version VERSION_LESS "3")
+    # This is a python2 requirement.
+    if(PYTHON_VERSION_STRING VERSION_LESS ${version})
+      message(FATAL_ERROR "${CMAKE_PROJECT_NAME} requires at least python ${version}")
+    endif()
+  else()
+    # This is a python3 requirement.
+    if(PYTHON3_VERSION_STRING VERSION_LESS ${version})
+      message(FATAL_ERROR "${CMAKE_PROJECT_NAME} requires at least python ${version}")
+    endif()
   endif()
 endmacro()
 
