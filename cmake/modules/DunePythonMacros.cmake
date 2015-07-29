385f9d6e30847f0335d73ecce88168dfbfd128b8
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-29 15:23:09 +0200

    Only build the virtualenvs if the respective interpreters have been found

--- cmake/modules/DunePythonMacros.cmake
+++ cmake/modules/DunePythonMacros.cmake
@@ -20,18 +20,22 @@ endif()
 # first non-installed module in the stack of modules to build.
 
 # The python2 virtualenv
-create_virtualenv(NAME python2-env
-                  ONLY_ONCE
-                  REAL_PATH DUNE_VIRTUALENV_PATH)
-create_virtualenv_wrapper(ENVPATH ${DUNE_VIRTUALENV_PATH}
-                          NAME dune-env)
-create_virtualenv_wrapper(ENVPATH ${DUNE_VIRTUALENV_PATH}
-                          NAME dune-env-2)
+if(PYTHON2INTERP_FOUND)
+  create_virtualenv(NAME python2-env
+                    ONLY_ONCE
+                    REAL_PATH DUNE_VIRTUALENV_PATH)
+  create_virtualenv_wrapper(ENVPATH ${DUNE_VIRTUALENV_PATH}
+                            NAME dune-env)
+  create_virtualenv_wrapper(ENVPATH ${DUNE_VIRTUALENV_PATH}
+                            NAME dune-env-2)
+endif()
 
 # The python3 virtualenv
-create_virtualenv(NAME python3-env
-                  ONLY_ONCE
-                  REAL_PATH DUNE_VIRTUALENV_PATH
-                  INTERPRETER ${PYTHON3_EXECUTABLE})
-create_virtualenv_wrapper(ENVPATH ${DUNE_VIRTUALENV_PATH}
-                          NAME dune-env-3)
+if(PYTHON3INTERP_FOUND)
+  create_virtualenv(NAME python3-env
+                    ONLY_ONCE
+                    REAL_PATH DUNE_VIRTUALENV_PATH
+                    INTERPRETER ${PYTHON3_EXECUTABLE})
+  create_virtualenv_wrapper(ENVPATH ${DUNE_VIRTUALENV_PATH}
+                            NAME dune-env-3)
+endif()
