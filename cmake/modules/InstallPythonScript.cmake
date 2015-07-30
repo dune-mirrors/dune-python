fc750cd1ff1a8759763d2d7c6a60f8f1ac5c42e3
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-07-30 11:35:35 +0200

    Allow passing requirements of python scripts
    and install them with pip.

--- cmake/modules/InstallPythonScript.cmake
+++ cmake/modules/InstallPythonScript.cmake
@@ -1,7 +1,8 @@
 # Macro to install a single executable python script
 #
 # dune_install_python_script(SCRIPT script [script2 ...]
-#                           [MAJOR_VERSION version])
+#                           [MAJOR_VERSION version]
+#                           [REQUIRES requ1 [requ2 ...]])
 #
 # Installs a script into the virtualenv(s) created by dune-python.
 # It is placed in the bin folder of the env. You should write
@@ -15,12 +16,17 @@
 # MAJOR_VERSION parameter. This will restrict the installation process to that
 # python version.
 #
+# If your script requires any non-dune python packages, pass them via the
+# REQUIRES parameter. They will be installed into the virtualenv
+# using pip and will be installed on the system during "make install".
+# Dune python packages are already present in the virtualenv.
+#
 
 function(dune_install_python_script)
   # Parse Arguments
   set(OPTION)
   set(SINGLE MAJOR_VERSION)
-  set(MULTI SCRIPT)
+  set(MULTI SCRIPT REQUIRES)
   include(CMakeParseArguments)
   cmake_parse_arguments(PYINST "${OPTION}" "${SINGLE}" "${MULTI}" ${ARGN})
   if(PYINST_UNPARSED_ARGUMENTS)
@@ -32,12 +38,36 @@ function(dune_install_python_script)
     set(PYINST_MAJOR_VERSION 2 3)
   endif()
 
+  # check for available pip packages
+  check_python_package(PACKAGE pip
+                       INTERPRETER ${PYTHON2_EXECUTABLE}
+                       RESULT PIP2_FOUND)
+  check_python_package(PACKAGE pip
+                       INTERPRETER ${PYTHON3_EXECUTABLE}
+                       RESULT PIP3_FOUND)
+
   foreach(version ${PYINST_MAJOR_VERSION})
+    # Install the requirements into the virtualenv
+    foreach(requ ${PYINST_REQUIRES})
+      execute_process(COMMAND ${CMAKE_BINARY_DIR}/dune-env-${version} pip install ${requ})
+    endforeach()
+
     # Install into the virtualenv(s)
     foreach(file ${PYINST_SCRIPT})
       # copy the file into the virtual env. The copy command is "cmake -E copy"
       execute_process(COMMAND ${CMAKE_BINARY_DIR}/dune-env-${version} ${CMAKE_COMMAND} -E copy ${file} $ENV{VIRTUAL_ENV}/bin)
     endforeach()
+
+    # Install the given requirements during make install
+    if(PIP${version}_FOUND)
+      set(USER_STRING "")
+      if(DUNE_PYTHON_INSTALL_USER)
+        set(USER_STRING "--user ${DUNE_PYTHON_INSTALL_USER}")
+      endif()
+      install(CODE "execute_process(COMMAND ${PYTHON${version}_EXECUTABLE} -m pip install ${USER_STRING} ${requ})")
+    else()
+      install(CODE "message(FATAL_ERROR \"You need the python${version} package pip installed on the host system to install requirements of script ${PYINST_SCRIPT}\")")
+    endif()
 
     # Mark it for global installation
     install(FILES ${PYINST_SCRIPT} DESTINATION ${CMAKE_INSTALL_BINDIR})
