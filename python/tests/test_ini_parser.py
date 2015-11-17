aa5b04758dc0276d24215e091d2e7ff5bc6bd3f6
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-11-17 13:50:37 +0100

    Move basic ini file handling into dune.common
    from dune.testtools

---
+++ python/tests/test_ini_parser.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from dune.common.ini.parser import parse_ini_file
+
+
+def test_parser(dir):
+    parsed = parse_ini_file(dir + "parse.ini")
+    assert(len(parsed) == 8)
+    assert(parsed['x'] == '5')
+    assert(parsed['y'] == 'str')
+    assert(parsed['group.y'] == 'str')
+    assert(parsed['group.x'] == '5')
+    assert(parsed['group.z'] == '1')
+    assert(parsed['group.subgroup.y'] == 'str')
+    assert(parsed['group.subgroup.z'] == '1')
