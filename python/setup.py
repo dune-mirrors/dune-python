300a2f891c8e8422a727131eece5a32c911c1ae4
Dominic Kempf <dominic.r.kempf@gmail.com>
2015-11-17 13:50:37 +0100

    Move basic ini file handling into dune.common
    from dune.testtools
    
    (cherry picked from commit aa5b04758dc0276d24215e091d2e7ff5bc6bd3f6)

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='dune.common',
-      namespace_packages = ['dune'],
+      namespace_packages=['dune'],
       version='2.4',
       description='Python package accompanying the DUNE project',
       url='http://www.dune-project.org',
       author='Dominic Kempf',
       author_email='dominic.kempf@iwr.uni-heidelberg.de',
       license='BSD',
-      packages=['dune.common']
-     )
+      packages=['dune.common', 'dune.common.ini'])
